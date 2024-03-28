baca2998ff3bdd89c020ee5e05d1ac53775838ae
PavelOnishenko <120336683+PavelOnishenko@users.noreply.github.com>
2024-03-28 21:42:46 +0100

    First prototype works.

--- Assets/Scripts/Countdown.cs
+++ Assets/Scripts/Countdown.cs
@@ -8,25 +8,32 @@ public class Countdown : MonoBehaviour
 {
     [SerializeField] private int countdownTime = 3;
     [SerializeField] private GameObject countdownContainer;
+
+    private GameObject[] digits;
     
     public void Restart() => StartCoroutine(CountdownCoroutine());
 
-    private void Start() => StartCoroutine(CountdownCoroutine());
+    private void Start()
+    {
+        digits = countdownContainer.GetComponentsInChildren<Transform>(true).Select(x => x.gameObject)
+            .Where(x => x.name.Contains("Label")).ToArray();
+        StartCoroutine(CountdownCoroutine());
+    }
 
     private IEnumerator CountdownCoroutine()
     {
-        var digits = countdownContainer.GetComponentsInChildren<Transform>(true).Select(x => x.gameObject)
-            .Where(x => x.name.Contains("Label")).ToArray();
-
-        
+        countdownTime = 3;
+        countdownContainer.SetActive(true);
         while (countdownTime > 0)
         {
-            GameObject[] activeDigits = digits.Where(x => x.activeSelf).ToArray();
+            Debug.Log($"CountdownTime == {countdownTime}.");
+            var activeDigits = digits.Where(x => x.activeSelf).ToArray();
             foreach (var t in activeDigits) t.gameObject.SetActive(false);
-
             var digitToShowName = countdownTime switch { 3 => "LabelThree", 2 => "LabelTwo", 1 => "LabelOne", _ => null };
+            Debug.Log($"digitToShowName == {digitToShowName}.");
             var digitToShow = digits.Single(x => x.name == digitToShowName);
             digitToShow.SetActive(true);
+            Debug.Log("digitToShow.SetActive(true)");
             yield return new WaitForSeconds(1f);
             countdownTime--;
         }
