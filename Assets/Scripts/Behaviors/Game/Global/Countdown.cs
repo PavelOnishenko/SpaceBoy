b93fa6caeec94786d0c059b178104cfd1598a5d9
PavelOnishenko <120336683+PavelOnishenko@users.noreply.github.com>
2024-05-26 20:52:12 +0200

    Returned to old Countdown implementation.

--- Assets/Scripts/Behaviors/Game/Global/Countdown.cs
+++ Assets/Scripts/Behaviors/Game/Global/Countdown.cs
@@ -3,95 +3,49 @@ using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
-// todo refactor this with either old or new approach
 public class Countdown : MonoBehaviour
 {
     [SerializeField] private int countdownTime = 3;
 
     private GameObject[] digits;
     private bool isCounting;
-    private float timeStamp;
 
-    //public void Restart() => StartCoroutine(CountdownCoroutine());
-    public void Restart() => LaunchCountdown();
+    public void Restart() => StartCoroutine(CountdownCoroutine());
 
     private void Start()
     {
         digits = GetComponentsInChildren<Transform>(true).Select(x => x.gameObject)
             .Where(x => x.name.Contains("Label")).ToArray();
-        //StartCoroutine(CountdownCoroutine());
-
-        // todo this is 26 05 2024
-        LaunchCountdown();
+        StartCoroutine(CountdownCoroutine());
     }
 
-    private void LaunchCountdown()
+    private IEnumerator CountdownCoroutine()
     {
-        countdownTime = 3;
-        timeStamp = Time.time;
+        if (isCounting) yield break;
         isCounting = true;
-    }
-
-    // todo this is 26 05 2024
-    private void Update()
-    {
-        if (isCounting)
+        countdownTime = 3;
+        gameObject.SetActive(true);
+        while (countdownTime > 0)
         {
-            var currentValue = 3 - Mathf.FloorToInt(Time.time - timeStamp);
-            if (countdownTime != currentValue)
-            {
-                countdownTime = currentValue;
-                if (countdownTime == 0)
-                {
-                    SetActive("LabelOne", false);
-                    SetActive("LabelGo", true);
-                    GameInfo.Instance.State = GameState.Ongoing;
-                }
-                else if(countdownTime == -1)
-                {
-                    SetActive("LabelGo", false);
-                    isCounting = false;
-                }
-                else
-                {
-                    SetDigitsActive();
-                }
-            }
+            SetDigitsActive();
+            yield return new WaitForSeconds(1f);
+            countdownTime--;
         }
+        SetActive("LabelOne", false);
+        SetActive("LabelGo", true);
+        yield return new WaitForSeconds(1f);
+        SetActive("LabelGo", false);
+        isCounting = false;
+        GameInfo.Instance.State = GameState.Ongoing;
     }
 
     private void SetDigitsActive()
     {
         var activeDigits = digits.Where(x => x.activeSelf).ToArray();
-        foreach (var digit in activeDigits) digit.SetActive(false);
-        var labelName = countdownTime switch
-        {
-            3 => "LabelThree", 2 => "LabelTwo", 1 => "LabelOne", 0 => "LabelGo",
-            _ => throw new System.Exception($"Can't convert number [{countdownTime}] to label name.")
-        };
-        SetActive(labelName, true);
+        foreach (var t in activeDigits) t.gameObject.SetActive(false);
+        SetActive(countdownTime switch { 3 => "LabelThree", 2 => "LabelTwo", 1 => "LabelOne", _ => null }, true);
     }
 
-    private void SetActive(string gameObjectName, bool newValue) => 
+    private void SetActive(string gameObjectName, bool newValue) =>
         digits.Single(x => x.name == gameObjectName).SetActive(newValue);
-
-    //private IEnumerator CountdownCoroutine()
-    //{
-    //    if (isCounting) yield break;
-    //    isCounting = true;
-    //    countdownTime = 3;
-    //    gameObject.SetActive(true);
-    //    while (countdownTime > 0)
-    //    {
-    //        SetDigitsActive();
-    //        yield return new WaitForSeconds(1f);
-    //        countdownTime--;
-    //    }
-    //    SetActive("LabelOne", false);
-    //    SetActive("LabelGo", true);
-    //    yield return new WaitForSeconds(1f);
-    //    gameObject.SetActive(false);
-    //    isCounting = false;
-    //    GameInfo.Instance.State = GameState.Ongoing;
-    //}
 }
