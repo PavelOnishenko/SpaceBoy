135bd24422e4390a1c9815a07a25d1d2b450a87a
PavelOnishenko <120336683+PavelOnishenko@users.noreply.github.com>
2024-10-14 22:56:16 +0200

    Mute sound button.

--- Assets/Scripts/Game/Sound/AudioManager.cs
+++ Assets/Scripts/Game/Sound/AudioManager.cs
@@ -6,6 +6,7 @@ public class AudioManager : MonoBehaviour
     public static AudioManager Instance { get; private set; }
 
     [SerializeField] private AudioSource audioSource;
+    [SerializeField] private Sound[] sounds;
 
     [System.Serializable]
     public class Sound
@@ -15,7 +16,6 @@ public class AudioManager : MonoBehaviour
         [Range(0f, 1f)] public float volume = 1f; 
     }
 
-    [SerializeField] private Sound[] sounds; 
     private Dictionary<string, Sound> soundDictionary;
 
     private void Awake()
@@ -31,17 +31,15 @@ public class AudioManager : MonoBehaviour
         }
 
         soundDictionary = new Dictionary<string, Sound>();
-        foreach (Sound sound in sounds)
-        {
+        foreach (var sound in sounds)
             soundDictionary.Add(sound.name, sound);
-        }
     }
 
     public void PlaySound(string soundName)
     {
         if (soundDictionary.ContainsKey(soundName))
         {
-            Sound sound = soundDictionary[soundName];
+            var sound = soundDictionary[soundName];
             audioSource.clip = sound.clip;
             audioSource.volume = sound.volume;
             audioSource.Play();
---
+++ Assets/Scripts/Menu/Buttons/MusicButton.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicButton : MonoBehaviour
+{
+    [SerializeField] private Sprite offSprite;
+    [SerializeField] private Sprite onSprite;
+
+    private bool isOn = true;
+    private Image imageComponent;
+
+    void Awake()
+    {
+        imageComponent = GetComponent<Image>();
+        isOn = !AudioListener.pause;
+        imageComponent.sprite = isOn ? onSprite : offSprite;
+    }
+
+    public void Click()
+    {
+        isOn = !isOn;
+        imageComponent.sprite = isOn ? onSprite : offSprite;
+        AudioListener.pause = !isOn;
+    }
+}
